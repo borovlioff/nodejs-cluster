f30fd48a45dc5055a7bc65113c2488e586db58d1
Aleksandr-glad <borovliov.alex@ya.ru>
2022-03-10 00:13:20 +0700

    Add reaction on signal

--- src/cluster.ts
+++ src/cluster.ts
@@ -1,30 +1,33 @@
 import cluster from "cluster";
-import {cpus} from "os";
+import { cpus } from "os";
 
 const pid = process.pid;
 
-if(cluster.isPrimary){
+if (cluster.isPrimary) {
     const cpusCount = cpus().length;
     console.log(`Master start. PID ${pid}\nCPU: ${cpusCount}`);
-    for(let i = 0 ; i < cpusCount-1; i++){
-       const worker = cluster.fork();
-       worker.on("exit", ()=>{
-           console.log(`Worker died! PID${worker.process.pid}`);
-           cluster.fork();
-       })
-       worker.send("Hello from server");
-       worker.on("message", (msg)=>{
-        console.log(`Message from worker ${worker.process.pid} : ${JSON.stringify(msg)}`);
-       });
+    for (let i = 0; i < cpusCount - 1; i++) {
+        const worker = cluster.fork();
+        worker.send("Hello from server");
+        worker.on("message", (msg) => {
+            console.log(`Message from worker ${worker.process.pid} : ${JSON.stringify(msg)}`);
+        });
     }
+
+    cluster.on("exit", (worker, code) => {
+        console.log(`Worker died! PID: ${worker.process.pid}. Code: ${code}`);
+        if (code === 1) {
+            cluster.fork();
+        }
+    })
 }
 
-if(cluster.isWorker){
+if (cluster.isWorker) {
     require("./worker");
-    process.on("message", (msg)=>{
+    process.on("message", (msg) => {
         console.log(`Message from master: ${msg}`)
     });
-    if(process.send){
-        process.send({text:"Hello", pid});
+    if (process.send) {
+        process.send({ text: "Hello", pid });
     };
 }
--- src/worker.ts
+++ src/worker.ts
@@ -4,9 +4,30 @@ const pid = process.pid;
 const PORT = process.env.PORT || 3000;
 
 
-createServer((req,res)=>{
+const server = createServer((req,res)=>{
     for(let i = 0; i < 1e7; i++){}
     res.end("Hello");
 }).listen(PORT, ()=>{
     console.log(`Server start \nPid: ${pid}`)
+});
+
+process.on("SIGINT", ()=>{
+    console.log(`SIGINT`);
+    server.close(()=>{
+        process.exit(0);
+    })
+})
+
+process.on("SIGTERM", ()=>{
+    console.log(`SIGTERM`);
+    server.close(()=>{
+        process.exit(0);
+    })
+})
+
+process.on("SIGUSR2", ()=>{
+    console.log(`SIGUSR2`);
+    server.close(()=>{
+        process.exit(1);
+    })
 })
